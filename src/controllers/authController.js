ac239cc6c7950635274814766368c8ef2b66fc14
Sreejay Shreekumar <sreejay.s@zerone-consulting.com>
2026-02-20 15:43:15 +0530

    US_01- Constant Message File Created and Response message Updated

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,6 +1,7 @@
 const common = require('../utilities/common');
 const responseHelper = require('../responseModels/apiResponseHelper');
 const userRepository = require('../repositories/authRepsoitory');
+const constantMessage = require('../utilities/constantMessage');
 
 
 const register = async (req, res) => {
@@ -8,12 +9,21 @@ const register = async (req, res) => {
         const { name, email, password } = req.body;
         const existingUser = await userRepository.findUserByEmail(email);
         if (existingUser) {
-            return responseHelper.getErrorResult('User already exists', res);
+            return responseHelper.getErrorResult(constantMessage.responseMessages.userExists, res);
         }
         const hashedPassword = await common.hashPassword(password);
         const newUser = await userRepository.createUser({ name, email, password: hashedPassword });
         const token = common.generateToken(newUser);
-        responseHelper.successCreate({ user: newUser, token }, res);
+        
+        const responseData = {
+            message: constantMessage.responseMessages.userCreated,
+                name: newUser.name,
+                email: newUser.email,
+                password: newUser.password,
+            token
+        };
+
+        responseHelper.successCreate(responseData, res);
     } catch (error) {   
         responseHelper.internalServerError(res, error.message);
     }
---
+++ src/utilities/constantMessage.js
@@ -0,0 +1,7 @@
+module.exports={
+    responseMessages: {
+        userCreated: 'User created successfully',
+        userExists: 'User with this email already exists',
+        internalServerError: 'Internal server error'
+    }
+}
